f7fdf4282694001458bfb59586389d5663164fab
Prajwal B M <bmprajwal43@gmail.com>
2024-11-07 22:16:00 +0530

    Complete Project

--- CurencyExchangeRate/src/App.jsx
+++ CurencyExchangeRate/src/App.jsx
@@ -1,8 +1,97 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { currencyCountryCodeMap } from './assets/CountryCode';
+
 function App() {
+  const [input, setInput] = useState(1);
+  const [currFrom, setCurrFrom] = useState("usd");
+  const [currTo, setCurrTo] = useState("inr");
+  const [currencyRate, setCurrencyRate] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const getCurrency = async () => {
+    try {
+      const response = await fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currFrom}.json`);
+      if (!response.ok) {
+        console.error("Network response was not ok!");
+        return;
+      }
+      const data = await response.json();
+      setCurrencyRate(data[currFrom][currTo]);
+      console.log(data[currFrom][currTo])
+    } catch (err) {
+      console.error("Error fetching data:", err);
+    }
+  };
+
+  useEffect(() => {
+    getCurrency();
+  }, [currFrom, currTo]);
+
+  useEffect(() => {
+    if (currencyRate !== null) {
+      setResult(input * currencyRate);
+    }
+  }, [input, currencyRate]);
+
+  const handleInputChange = (e) => setInput(e.target.value);
+  const handleFromCurrencyChange = (e) => setCurrFrom(e.target.value.toLowerCase());
+  const handleToCurrencyChange = (e) => setCurrTo(e.target.value.toLowerCase());
+
   return (
     <div className="container">
-      App page
+      <h1>Currency Converter</h1>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className="amount">
+          <p>Enter Amount</p>
+          <input
+            type="number"
+            min={1}
+            value={input}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="dropdown">
+          <div className="from">
+            <p>From</p>
+            <div className="select-container">
+              <img src={`https://flagsapi.com/${currencyCountryCodeMap[currFrom.toUpperCase()]}/flat/64.png`} alt={`${currFrom}`} />
+              <select value={currFrom.toUpperCase()} onChange={handleFromCurrencyChange}>
+                {Object.keys(currencyCountryCodeMap).map((code) => (
+                  <option key={code} value={code}>
+                    {code}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <div className="to">
+            <p>To</p>
+            <div className="select-container">
+              <img src={`https://flagsapi.com/${currencyCountryCodeMap[currTo.toUpperCase()]}/flat/64.png`} alt={`${currTo}`} />
+              <select value={currTo.toUpperCase()} onChange={handleToCurrencyChange}>
+                {Object.keys(currencyCountryCodeMap).map((code) => (
+                  <option key={code} value={code}>
+                    {code}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="msg">
+          {currencyRate !== null  && result != null
+            ? `${input} ${currFrom.toUpperCase()} = ${result.toFixed(2)} ${currTo.toUpperCase()}`
+            : "Loading exchange rate..."}
+        </div>
+
+      </form>
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
---
+++ CurencyExchangeRate/src/assets/CountryCode.js
@@ -0,0 +1,163 @@
+export const currencyCountryCodeMap = {
+    AED: "AE", // United Arab Emirates Dirham
+    AFN: "AF", // Afghan Afghani
+    ALL: "AL", // Albanian Lek
+    AMD: "AM", // Armenian Dram
+    ANG: "NL", // Netherlands Antillean Guilder (Curaçao and Sint Maarten)
+    AOA: "AO", // Angolan Kwanza
+    ARS: "AR", // Argentine Peso
+    AUD: "AU", // Australian Dollar
+    AWG: "AW", // Aruban Florin
+    AZN: "AZ", // Azerbaijani Manat
+    BAM: "BA", // Bosnia-Herzegovina Convertible Mark
+    BBD: "BB", // Barbadian Dollar
+    BDT: "BD", // Bangladeshi Taka
+    BGN: "BG", // Bulgarian Lev
+    BHD: "BH", // Bahraini Dinar
+    BIF: "BI", // Burundian Franc
+    BMD: "BM", // Bermudian Dollar
+    BND: "BN", // Brunei Dollar
+    BOB: "BO", // Bolivian Boliviano
+    BRL: "BR", // Brazilian Real
+    BSD: "BS", // Bahamian Dollar
+    BTN: "BT", // Bhutanese Ngultrum
+    BWP: "BW", // Botswanan Pula
+    BYN: "BY", // Belarusian Ruble
+    BZD: "BZ", // Belize Dollar
+    CAD: "CA", // Canadian Dollar
+    CDF: "CD", // Congolese Franc
+    CHF: "CH", // Swiss Franc
+    CLP: "CL", // Chilean Peso
+    CNY: "CN", // Chinese Yuan
+    COP: "CO", // Colombian Peso
+    CRC: "CR", // Costa Rican Colón
+    CUP: "CU", // Cuban Peso
+    CVE: "CV", // Cape Verdean Escudo
+    CZK: "CZ", // Czech Koruna
+    DJF: "DJ", // Djiboutian Franc
+    DKK: "DK", // Danish Krone
+    DOP: "DO", // Dominican Peso
+    DZD: "DZ", // Algerian Dinar
+    EGP: "EG", // Egyptian Pound
+    ERN: "ER", // Eritrean Nakfa
+    ETB: "ET", // Ethiopian Birr
+    EUR: "EU", // Euro (multiple countries in Europe)
+    FJD: "FJ", // Fijian Dollar
+    FKP: "FK", // Falkland Islands Pound
+    FOK: "FO", // Faroese Króna
+    GBP: "GB", // British Pound
+    GEL: "GE", // Georgian Lari
+    GGP: "GG", // Guernsey Pound
+    GHS: "GH", // Ghanaian Cedi
+    GIP: "GI", // Gibraltar Pound
+    GMD: "GM", // Gambian Dalasi
+    GNF: "GN", // Guinean Franc
+    GTQ: "GT", // Guatemalan Quetzal
+    GYD: "GY", // Guyanese Dollar
+    HKD: "HK", // Hong Kong Dollar
+    HNL: "HN", // Honduran Lempira
+    HRK: "HR", // Croatian Kuna
+    HTG: "HT", // Haitian Gourde
+    HUF: "HU", // Hungarian Forint
+    IDR: "ID", // Indonesian Rupiah
+    ILS: "IL", // Israeli New Shekel
+    IMP: "IM", // Isle of Man Pound
+    INR: "IN", // Indian Rupee
+    IQD: "IQ", // Iraqi Dinar
+    IRR: "IR", // Iranian Rial
+    ISK: "IS", // Icelandic Króna
+    JEP: "JE", // Jersey Pound
+    JMD: "JM", // Jamaican Dollar
+    JOD: "JO", // Jordanian Dinar
+    JPY: "JP", // Japanese Yen
+    KES: "KE", // Kenyan Shilling
+    KGS: "KG", // Kyrgyzstani Som
+    KHR: "KH", // Cambodian Riel
+    KID: "KI", // Kiribati Dollar
+    KMF: "KM", // Comorian Franc
+    KRW: "KR", // South Korean Won
+    KWD: "KW", // Kuwaiti Dinar
+    KYD: "KY", // Cayman Islands Dollar
+    KZT: "KZ", // Kazakhstani Tenge
+    LAK: "LA", // Laotian Kip
+    LBP: "LB", // Lebanese Pound
+    LKR: "LK", // Sri Lankan Rupee
+    LRD: "LR", // Liberian Dollar
+    LSL: "LS", // Lesotho Loti
+    LYD: "LY", // Libyan Dinar
+    MAD: "MA", // Moroccan Dirham
+    MDL: "MD", // Moldovan Leu
+    MGA: "MG", // Malagasy Ariary
+    MKD: "MK", // Macedonian Denar
+    MMK: "MM", // Myanmar Kyat
+    MNT: "MN", // Mongolian Tögrög
+    MOP: "MO", // Macanese Pataca
+    MRU: "MR", // Mauritanian Ouguiya
+    MUR: "MU", // Mauritian Rupee
+    MVR: "MV", // Maldivian Rufiyaa
+    MWK: "MW", // Malawian Kwacha
+    MXN: "MX", // Mexican Peso
+    MYR: "MY", // Malaysian Ringgit
+    MZN: "MZ", // Mozambican Metical
+    NAD: "NA", // Namibian Dollar
+    NGN: "NG", // Nigerian Naira
+    NIO: "NI", // Nicaraguan Córdoba
+    NOK: "NO", // Norwegian Krone
+    NPR: "NP", // Nepalese Rupee
+    NZD: "NZ", // New Zealand Dollar
+    OMR: "OM", // Omani Rial
+    PAB: "PA", // Panamanian Balboa
+    PEN: "PE", // Peruvian Sol
+    PGK: "PG", // Papua New Guinean Kina
+    PHP: "PH", // Philippine Peso
+    PKR: "PK", // Pakistani Rupee
+    PLN: "PL", // Polish Złoty
+    PYG: "PY", // Paraguayan Guarani
+    QAR: "QA", // Qatari Riyal
+    RON: "RO", // Romanian Leu
+    RSD: "RS", // Serbian Dinar
+    RUB: "RU", // Russian Ruble
+    RWF: "RW", // Rwandan Franc
+    SAR: "SA", // Saudi Riyal
+    SBD: "SB", // Solomon Islands Dollar
+    SCR: "SC", // Seychellois Rupee
+    SDG: "SD", // Sudanese Pound
+    SEK: "SE", // Swedish Krona
+    SGD: "SG", // Singapore Dollar
+    SHP: "SH", // Saint Helena Pound
+    SLL: "SL", // Sierra Leonean Leone
+    SOS: "SO", // Somali Shilling
+    SRD: "SR", // Surinamese Dollar
+    SSP: "SS", // South Sudanese Pound
+    STN: "ST", // São Tomé and Príncipe Dobra
+    SYP: "SY", // Syrian Pound
+    SZL: "SZ", // Swazi Lilangeni
+    THB: "TH", // Thai Baht
+    TJS: "TJ", // Tajikistani Somoni
+    TMT: "TM", // Turkmenistani Manat
+    TND: "TN", // Tunisian Dinar
+    TOP: "TO", // Tongan Paʻanga
+    TRY: "TR", // Turkish Lira
+    TTD: "TT", // Trinidad and Tobago Dollar
+    TVD: "TV", // Tuvaluan Dollar
+    TWD: "TW", // New Taiwan Dollar
+    TZS: "TZ", // Tanzanian Shilling
+    UAH: "UA", // Ukrainian Hryvnia
+    UGX: "UG", // Ugandan Shilling
+    USD: "US", // United States Dollar
+    UYU: "UY", // Uruguayan Peso
+    UZS: "UZ", // Uzbekistani Som
+    VES: "VE", // Venezuelan Bolívar
+    VND: "VN", // Vietnamese Đồng
+    VUV: "VU", // Vanuatu Vatu
+    WST: "WS", // Samoan Tala
+    XAF: "XA", // Central African CFA Franc
+    XCD: "XC", // East Caribbean Dollar
+    XOF: "XO", // West African CFA Franc
+    XPF: "XP", // CFP Franc
+    YER: "YE", // Yemeni Rial
+    ZAR: "ZA", // South African Rand
+    ZMW: "ZM", // Zambian Kwacha
+    ZWL: "ZW"  // Zimbabwean Dollar
+  };
+  
